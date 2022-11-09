79f111a3c9cf666ac5718bf201b3c750b2a90ab8
Habibur Rahman <habibrajbd24@gmail.com>
2022-11-09 16:00:30 +0600

    responsive about section created

--- js/main.js
+++ js/main.js
@@ -1,5 +1,21 @@
+// hamburger menu functionality
 const menu = document.querySelector(".menuIconHolder");
 
 menu.addEventListener("click", () => {
   menu.classList.toggle("openMenu");
 });
+
+//about tab functionality
+const tabLinks = document.getElementsByClassName("tabLinks");
+const tabContents = document.getElementsByClassName("tabContents");
+
+function openTab(element) {
+  for (tabLink of tabLinks) {
+    tabLink.classList.remove("activeLinks");
+  }
+  for (tabContent of tabContents) {
+    tabContent.classList.remove("activeTab");
+  }
+  event.currentTarget.classList.add("activeLinks");
+  document.getElementById(element).classList.add("activeTab");
+}
